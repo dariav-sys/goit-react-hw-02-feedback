3b53aa92787c6f96cf71b752e3de47d4edf1d539
Daria Viter <viter.daria@gmail.com>
2021-02-25 19:18:18 +0000

    решение

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Section from './section/Section.js';
+import Statistics from "./statistics/Statistics.js";
+import FeedbackOptions from "./feedbackOptions/FeedbackOptions.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+  }
+
+  leaveFeedback = (event) => {
+    const feedbackValue = event.target.name;
+    this.setState(prevState => {
+      return {
+        [feedbackValue]: prevState[feedbackValue] + 1,
+      }
+    })
+  }
+
+  countTotalFeedback(){
+    const { good, neutral, bad } = this.state;
+    return good + bad + neutral;
+  }
+
+  countPositiveFeedbackPercentage(){
+    return Math.floor(this.state.good / this.countTotalFeedback() * 100);
+  }
+
+  render() {
+    const options = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Section title="Please provide your feedback :)" />
+        <FeedbackOptions options={options} onLeaveFeedback={ this.leaveFeedback}/>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback()}
+          positiveFeedbackPercent={this.countPositiveFeedbackPercentage() }/>
+        
+      </>
+    )
+  }
 }
 
 export default App;
